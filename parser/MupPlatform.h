cde0bd902525cc6709e1378882345596d3bd4a0a
zhangc <zhangc.simzc@outlook.com>
2023-09-26 13:54:06 +0800

    🐞 fix(cmake): api compilation
    1. modified the condition of macro to determain the compilation platform.
    
    Chao

--- parser/MupPlatform.h
+++ parser/MupPlatform.h
@@ -3,7 +3,7 @@
 
 // Functionality for API import/export symbols, in a platform independent way.
 // When building the DLL, use the MupApiEXPORT macro. When using the DLL, use the MupApiIMPORT macro.
-#if ((defined _WIN32) || (defined(__MINGW32__) || defined(__CYGWIN__))) && defined(MUP_DLL)
+#if ((defined _WIN32) || (defined(__MINGW32__) || defined(__CYGWIN__))) && !defined(MUP_STATIC)
 #define MupApiEXPORT __declspec(dllexport)
 #define MupApiIMPORT __declspec(dllimport)
 #else
